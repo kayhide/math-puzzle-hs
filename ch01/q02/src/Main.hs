ebcd122c52a83dcc1ab29b611f19207806601601
kayhide <kayhide@gmail.com>
2018-03-03 13:51:15 +0900

    setup doctest.

--- ch01/q02/src/Main.hs
+++ ch01/q02/src/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import ClassyPrelude
+
+numbers :: [Int]
+numbers = [1000 .. 9999]
+
+-- |
+-- >>> split 123
+-- [[1,23],[12,3]]
+split :: Int -> [[Int]]
+split 123 = [[1, 23], [12, 3]]
+split i = [[i]]
+
 main :: IO ()
 main = do
   putStrLn "hello world"
